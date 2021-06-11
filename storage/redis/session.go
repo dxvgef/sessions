a3e421ac3689697349ad393520352f342fbb1fc6
dxvgef <dxvgef@outlook.com>
2021-06-11 16:33:28 +0800

    fix bug

--- storage/redis/session.go
+++ storage/redis/session.go
@@ -10,12 +10,13 @@ import (
 )
 
 func (rs *Storage) Add(id, key string, value string) (err error) {
+	var result bool
 	if err = rs.Connect(); err != nil {
 		return
 	}
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-	result, err := rs.redisClient.HSetNX(ctx, rs.config.Prefix+":"+id, key, value).Result()
+	result, err = rs.redisClient.HSetNX(ctx, rs.config.Prefix+":"+id, key, value).Result()
 	if err != nil {
 		return
 	}
@@ -26,36 +27,22 @@ func (rs *Storage) Add(id, key string, value string) (err error) {
 }
 
 func (rs *Storage) Delete(id, key string) (err error) {
-	var count int64
 	if err = rs.Connect(); err != nil {
 		return
 	}
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-	count, err = rs.redisClient.HDel(ctx, rs.config.Prefix+":"+id, key).Result()
-	if err != nil {
-		return
-	}
-	if count < 1 {
-		return errors.New("delete操作失败")
-	}
+	_, err = rs.redisClient.HDel(ctx, rs.config.Prefix+":"+id, key).Result()
 	return
 }
 
 func (rs *Storage) Put(id, key string, value string) (err error) {
-	var count int64
 	if err = rs.Connect(); err != nil {
 		return
 	}
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-	count, err = rs.redisClient.HSet(ctx, rs.config.Prefix+":"+id, key, value).Result()
-	if err != nil {
-		return
-	}
-	if count < 1 {
-		return errors.New("put操作失败")
-	}
+	_, err = rs.redisClient.HSet(ctx, rs.config.Prefix+":"+id, key, value).Result()
 	return
 }
 
@@ -72,11 +59,7 @@ func (rs *Storage) Update(id, key string, value string) (err error) {
 	if !result {
 		return errors.New("nil")
 	}
-	err = rs.redisClient.HSet(ctx, rs.config.Prefix+":"+id, key, value).Err()
-	if err != nil {
-		return
-	}
-	return
+	return rs.redisClient.HSet(ctx, rs.config.Prefix+":"+id, key, value).Err()
 }
 
 func (rs *Storage) Get(id, key string) (result sessions.Result) {
@@ -102,26 +85,15 @@ func (rs *Storage) Refresh(id string, expires time.Time) (err error) {
 	}
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-	err = rs.redisClient.ExpireAt(ctx, rs.config.Prefix+":"+id, expires).Err()
-	if err != nil {
-		return
-	}
-	return
+	return rs.redisClient.ExpireAt(ctx, rs.config.Prefix+":"+id, expires).Err()
 }
 
 func (rs *Storage) Destroy(id string) (err error) {
-	var count int64
 	if err = rs.Connect(); err != nil {
 		return
 	}
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-	count, err = rs.redisClient.Del(ctx, rs.config.Prefix+":"+id).Result()
-	if err != nil {
-		return
-	}
-	if count < 1 {
-		return errors.New("destroy操作失败")
-	}
+	_, err = rs.redisClient.Del(ctx, rs.config.Prefix+":"+id).Result()
 	return
 }
